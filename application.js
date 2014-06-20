3391ebcf81a7c2ac899a7098f064b615f96c61fa
Phil <philoxrud@gmail.com>
2014-06-20 14:58:12 -0400

    Initial Commit

--- application.js
+++ application.js
@@ -1,18 +1,46 @@
-var myApp = angular.module('MyApp', []);
+var myApp = angular.module('YouTubeApp', []);
 
-myApp.directive('youtube', function($window) {
+myApp.constant('YT_event', {
+  STOP:            0, 
+  PLAY:            1,
+  PAUSE:           2,
+  STATUS_CHANGE:   3
+});
+
+myApp.controller('YouTubeCtrl', function($scope, YT_event) {
+  //initial settings
+  $scope.yt = {
+    width: 600, 
+    height: 480, 
+    videoid: "M7lc1UVf-VE",
+    playerStatus: "NOT PLAYING"
+  };
+
+  $scope.YT_event = YT_event;
+
+  $scope.sendControlEvent = function (ctrlEvent) {
+    this.$broadcast(ctrlEvent);
+  }
+
+  $scope.$on(YT_event.STATUS_CHANGE, function(event, data) {
+      $scope.yt.playerStatus = data;
+  });
+
+});
+
+myApp.directive('youtube', function($window, YT_event) {
   return {
     restrict: "E",
 
     scope: {
       height: "@",
       width: "@",
-      videoid: "@",
+      videoid: "@"
     },
 
     template: '<div></div>',
 
-    link: function(scope, element, attrs) {
+    link: function(scope, element, attrs, $rootScope) {
       var tag = document.createElement('script');
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -22,23 +50,84 @@ myApp.directive('youtube', function($window) {
 
       $window.onYouTubeIframeAPIReady = function() {
 
-        var player = new YT.Player(element.children()[0], {
+        player = new YT.Player(element.children()[0], {
           playerVars: {
             autoplay: 0,
-            html5:1,
+            html5: 1,
             theme: "light",
             modesbranding: 0,
             color: "white",
             iv_load_policy: 3,
             showinfo: 1,
-            controls: 1,
+            controls: 1
           },
           
           height: scope.height,
           width: scope.width,
-          videoId: scope.videoid,  
+          videoId: scope.videoid, 
+
+          events: {
+            'onStateChange': function(event) {
+              
+              var message = {
+                event: YT_event.STATUS_CHANGE,
+                data: ""
+              };
+              
+              switch(event.data) {
+                case YT.PlayerState.PLAYING:
+                  message.data = "PLAYING";
+                  break;
+                case YT.PlayerState.ENDED:
+                  message.data = "ENDED";
+                  break;
+                case YT.PlayerState.UNSTARTED:
+                  message.data = "NOT PLAYING";
+                  break;
+                case YT.PlayerState.PAUSED:
+                  message.data = "PAUSED";
+                  break;
+              }
+
+              scope.$apply(function() {
+                scope.$emit(message.event, message.data);
+              });
+            }
+          } 
         });
-      }
+      };
+
+      scope.$watch('height + width', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+        
+        player.setSize(scope.width, scope.height);
+      
+      });
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+        
+        player.cueVideoById(scope.videoid);
+      
+      });
+
+      scope.$on(YT_event.STOP, function () {
+        player.seekTo(0);
+        player.stopVideo();
+      });
+
+      scope.$on(YT_event.PLAY, function () {
+        player.playVideo();
+      }); 
+
+      scope.$on(YT_event.PAUSE, function () {
+        player.pauseVideo();
+      });  
+
     }  
   };
 });
